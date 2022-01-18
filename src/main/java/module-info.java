24d24d48262598fd5634504f3aa526b8ac1d2401
ajaydeta <ajaydeta02@gmail.com>
2022-01-18 19:53:27 +0700

    init app

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.tiketly.tiketly {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.tiketly.tiketly to javafx.fxml;
+    exports com.tiketly.tiketly;
+}
