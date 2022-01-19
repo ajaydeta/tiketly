5c6f57fd289172f33f0ad70e20a0041d03ec3ba7
ajaydeta <ajaydeta02@gmail.com>
2022-01-19 10:01:09 +0700

    CHANGE - text field to passwordField di login'

--- src/main/java/com/tiketly/tiketly/controller/Login.java
+++ src/main/java/com/tiketly/tiketly/controller/Login.java
@@ -2,15 +2,21 @@ package com.tiketly.tiketly.controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class Login {
     public Button btnMasuk;
     public Button btnKembali;
     public TextField usernameField;
-    public TextField passwordField;
+    public PasswordField passwordField;
 
     public void masuk(ActionEvent actionEvent) {
+        String username = usernameField.getText();
+        String pass = passwordField.getText();
+
+        System.out.println("user :"+username);
+        System.out.println("pass :"+pass);
     }
 
     public void kembali(ActionEvent actionEvent) {
