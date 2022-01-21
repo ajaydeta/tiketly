96526ac204cfccf388a8fbc5f4c150870d78d66b
ajaydeta <ajaydeta02@gmail.com>
2022-01-21 10:17:34 +0700

    ADD - responsive jadwal sceene

--- src/main/java/com/tiketly/tiketly/controller/Jadwal.java
+++ src/main/java/com/tiketly/tiketly/controller/Jadwal.java
@@ -4,8 +4,7 @@ import helper.DataTravel;
 import helper.Navigation;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.Map;
@@ -17,13 +16,22 @@ public class Jadwal implements Initializable {
     public Button btnTIket;
     public Button btnJadwal;
     public TableView tableJadwalFilm;
+    public TableColumn filmCol;
+    public TableColumn bioskopCol;
+    public TableColumn jadwalCol;
+    public TableColumn noCol;
+    public TableColumn pukulCol;
+    public TextField namaFilmField;
+    public TextField bioskopField;
+    public DatePicker dateField;
+    public TextField timeField;
+    public Button btnSimpan;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         DataTravel dataTravel = DataTravel.getInstance();
         Map<String, String> data = dataTravel.getData();
         System.out.println("data");
-        System.out.println(data.get("user"));
     }
 
     public void jadwalOnClick(ActionEvent actionEvent) {
@@ -36,5 +44,8 @@ public class Jadwal implements Initializable {
     }
 
     public void penggunaOnClick(ActionEvent actionEvent) {
+    }
+
+    public void simpanOnClick(ActionEvent actionEvent) {
     }
 }
