d29937ecc1d83149114d5d1d619412e1c4946560
Ajay Deta <ajaydeta02@gmail.com>
2022-01-31 21:58:33 +0700

    pembelian

--- src/main/java/com/tiketly/tiketly/controller/adminController/Pembelian.java
+++ src/main/java/com/tiketly/tiketly/controller/adminController/Pembelian.java
@@ -1,4 +1,20 @@
 package com.tiketly.tiketly.controller.adminController;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+
 public class Pembelian extends AdminBase {
+    public TableView tablePembelian;
+    public ComboBox filterKasir;
+    public ComboBox filterBioskop;
+    public DatePicker filterTanggalAwal;
+    public DatePicker filterTanggalAkhir;
+
+    public void openFilterPembelian(ActionEvent actionEvent) {
+    }
+
+    public void useFilter(ActionEvent actionEvent) {
+    }
 }
