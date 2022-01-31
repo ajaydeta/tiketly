6cdd96fed0456cf72dd700f6ad66b9f2f7c3d079
Ajay Deta <ajaydeta02@gmail.com>
2022-01-31 21:48:04 +0700

    jadwal film

---
+++ src/main/java/com/tiketly/tiketly/controller/adminController/JadwalFilm.java
@@ -0,0 +1,25 @@
+package com.tiketly.tiketly.controller.adminController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class JadwalFilm extends AdminBase{
+    public TableView tableJadwal;
+    public ChoiceBox judul;
+    public ChoiceBox bioskop;
+    public ChoiceBox teater;
+    public DatePicker tanggalTayang;
+    public TextField jamTayang;
+    public DatePicker filterTanggalAkhir;
+    public DatePicker filterTanggalAwal;
+    public ComboBox filterBioskop;
+
+    public void simapanJadwal(ActionEvent actionEvent) {
+    }
+
+    public void openFilterJadwal(ActionEvent actionEvent) {
+    }
+
+    public void useFilter(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/tiketly/tiketly/controller/adminController/KelolaJadwal.java
+++
@@ -1,4 +0,0 @@
-package com.tiketly.tiketly.controller.adminController;
-
-public class KelolaJadwal extends AdminBase{
-}
