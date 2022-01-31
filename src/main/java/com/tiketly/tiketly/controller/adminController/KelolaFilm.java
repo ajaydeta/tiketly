7375cd04a50f70c301ef47cf44cb34181f19de45
Ajay Deta <ajaydeta02@gmail.com>
2022-01-31 20:45:38 +0700

    kelola film

--- src/main/java/com/tiketly/tiketly/controller/adminController/KelolaFilm.java
+++ src/main/java/com/tiketly/tiketly/controller/adminController/KelolaFilm.java
@@ -1,4 +1,22 @@
 package com.tiketly.tiketly.controller.adminController;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class KelolaFilm extends AdminBase{
+    public ChoiceBox filterSensor;
+    public TextField search;
+    public TextArea sinopsis;
+    public TextField judul;
+    public ChoiceBox sensor;
+    public TableView tableFilm;
+
+    public void useFilter(ActionEvent actionEvent) {
+    }
+
+    public void simapanFilm(ActionEvent actionEvent) {
+    }
 }
